09267a005fd29ee34de1821053fe1a0fe317bbd7
arijknani <118684147+arijknani@users.noreply.github.com>
2024-05-23 18:47:04 +0300

    Update deployOpenshift.groovy

--- vars/deployOpenshift.groovy
+++ vars/deployOpenshift.groovy
@@ -1,15 +1,15 @@
 def call(Map config = [:]) {
-    sh "oc project ${openshift_project}"
-    def deploymentExists = sh(script: "oc get deploy/${app_name}", returnStatus: true) == 0
+    sh "oc project ${config.openshift_project}"
+    def deploymentExists = sh(script: "oc get deploy/${config.app_name}", returnStatus: true) == 0
     if (deploymentExists) {
-        echo "Deployment ${app_name} exists, refreshing app..."
-        sh "oc set triggers deploy/${app_name} --from-image=${app_name}:latest"
-        sh "oc rollout restart deploy/${app_name}"
+        echo "Deployment ${config.app_name} exists, refreshing app..."
+        sh "oc set triggers deploy/${config.app_name} --from-image=${config.app_name}:latest"
+        sh "oc rollout restart deploy/${config.app_name}"
     } else {
-        echo "Deployment ${app_name} does not exist, deploying app..."
-        sh "oc new-app --image=quay.io/${quay_repo}/${image_name} --name=${app_name}"
-        sh "oc set env --from=secret/app-secrets deploy/${app_name}"
-        sh "oc set env --from=configmap/app-configmap deploy/${app_name}"
-        sh "oc expose svc/${app_name}"
+        echo "Deployment ${config.app_name} does not exist, deploying app..."
+        sh "oc new-app --image=quay.io/${config.quay_repo}/${config.image_name} --name=${config.app_name}"
+        sh "oc set env --from=secret/app-secrets deploy/${config.app_name}"
+        sh "oc set env --from=configmap/app-configmap deploy/${config.app_name}"
+        sh "oc expose svc/${config.app_name}"
     }
 }
