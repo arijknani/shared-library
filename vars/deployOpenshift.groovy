ae44a18d096199844eb10aa6b44ce4570b2c9ba9
arijknani <118684147+arijknani@users.noreply.github.com>
2024-05-23 19:37:31 +0300

    Update deployOpenshift.groovy

--- vars/deployOpenshift.groovy
+++ vars/deployOpenshift.groovy
@@ -3,7 +3,7 @@ def call(Map config = [:]) {
     def deploymentExists = sh(script: "oc get deploy/${config.app_name}", returnStatus: true) == 0
     if (deploymentExists) {
         echo "Deployment ${config.app_name} exists, refreshing app..."
-        sh "oc set triggers deploy/${config.app_name} --from-image=${config.app_name}:latest"
+        sh "oc set triggers deploy/${config.app_name} --from-image=${config.app_name}:latest -c ${config.app_name} "
         sh "oc rollout restart deploy/${config.app_name}"
     } else {
         echo "Deployment ${config.app_name} does not exist, deploying app..."
