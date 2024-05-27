4f7071c385b96ace2462baf18365e95d45a2404e
arijknani <118684147+arijknani@users.noreply.github.com>
2024-05-27 15:37:49 +0300

    Update deployOpenshift.groovy

--- vars/deployOpenshift.groovy
+++ vars/deployOpenshift.groovy
@@ -3,13 +3,14 @@ def call(Map config = [:]) {
     def deploymentExists = sh(script: "oc get deploy/${config.app_name}", returnStatus: true) == 0
     if (deploymentExists) {
         echo "Deployment ${config.app_name} exists, refreshing app..."
-        sh "oc set triggers deploy/${config.app_name} --from-image=${config.app_name}:latest -c ${config.app_name} "
-        sh "oc rollout restart deploy/${config.app_name}"
+        sh "oc set image deployment/${config.app_name} ${config.app_name}=${config.app_name}:latest"
+        sh "oc rollout restart deployment/${config.app_name}"
     } else {
         echo "Deployment ${config.app_name} does not exist, deploying app..."
         sh "oc new-app --image=quay.io/${config.quay_repo}/${config.image_name} --name=${config.app_name}"
         sh "oc set env --from=secret/app-secrets deploy/${config.app_name}"
         sh "oc set env --from=configmap/app-configmap deploy/${config.app_name}"
         sh "oc expose svc/${config.app_name}"
+        sh "oc set triggers deploy/${config.app_name} --from-image=${config.app_name}:latest -c ${config.app_name}"
     }
 }
